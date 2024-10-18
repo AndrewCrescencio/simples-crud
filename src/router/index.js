ff9fd787a4ceaf27795b8a5575b5b8674f2c8ba1
AndrewCrescencio <andrew_workstation@hotmail.com>
2024-10-18 15:57:24 -0300

    fix: new routes

--- src/router/index.js
+++ src/router/index.js
@@ -14,12 +14,14 @@ const router = new VueRouter({
       component: HomeView,
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      path: "/clients",
+      name: "clients",
+      component: () => import("../views/ClientsView.vue"),
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: () => import("../views/RegisterView.vue"),
     },
   ],
 });
