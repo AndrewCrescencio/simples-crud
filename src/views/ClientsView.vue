e80579e4e2eaf812fc61b7f59fc564f5546e2682
AndrewCrescencio <andrew_workstation@hotmail.com>
2024-10-18 15:55:57 -0300

    feat: ClientsView page component

---
+++ src/views/ClientsView.vue
@@ -0,0 +1,24 @@
+<script>
+import TableCustomers from "@/components/TableCustomers.vue";
+
+export default {
+  components: { TableCustomers },
+};
+</script>
+<template>
+  <main>
+    <h2 class="title">Clientes</h2>
+    <TableCustomers />
+  </main>
+</template>
+<style lang="scss" scoped>
+main {
+  max-width: 1440px;
+  margin: 0 auto;
+  padding: 0 16px;
+  padding-top: calc(var(--section-gap) / 4);
+  .title {
+    margin-bottom: calc(var(--section-gap) / 4);
+  }
+}
+</style>
