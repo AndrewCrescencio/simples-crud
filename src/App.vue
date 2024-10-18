baa62a99e3b386fd61f2c8b383c380e3214036f2
AndrewCrescencio <andrew_workstation@hotmail.com>
2024-10-18 15:58:45 -0300

    fix: app header TODO: create component AppHeader

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,35 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
   <div id="app">
-    <header>
-      <img
-        alt="Vue logo"
-        class="logo"
-        src="@/assets/logo.svg"
-        width="125"
-        height="125"
-      />
-
-      <div class="wrapper">
-        <HelloWorld msg="You did it!" />
-
-        <nav>
+    <header class="app-header">
+      <div class="app-header__wrapper">
+        <nav class="app-navbar">
           <router-link to="/">Home</router-link>
-          <router-link to="/about">About</router-link>
+          <router-link to="/clients">Clientes</router-link>
+          <router-link to="/register">Cadastrar</router-link>
         </nav>
       </div>
     </header>
-
     <router-view />
   </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style lang="scss" scoped>
+.app-header {
+  background-color: var(--color-background-soft);
+  &__wrapper {
+    width: 100%;
+    max-width: 1440px;
+    margin: 0 auto;
+  }
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
+.app-navbar {
+  padding: 0 16px;
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  height: 64px;
+  display: flex;
+  align-items: center;
+  gap: 16px;
 }
 
 nav a.router-link-exact-active {
@@ -51,42 +38,5 @@ nav a.router-link-exact-active {
 
 nav a.router-link-exact-active:hover {
   background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
