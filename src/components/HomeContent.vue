1f9c47d383020a8626a5bdbcec7d335303fa1148
AndrewCrescencio <andrew_workstation@hotmail.com>
2024-10-18 15:55:01 -0300

    feat: HomeContent component

---
+++ src/components/HomeContent.vue
@@ -0,0 +1,25 @@
+<script>
+import TheCard from "@/components/TheCard.vue";
+
+export default {
+  components: { TheCard },
+};
+</script>
+
+<template>
+  <section>
+    <div class="container">
+      <the-card title="Clientes" class="card" />
+    </div>
+  </section>
+</template>
+<style lang="scss" scoped>
+.container {
+  max-width: 1440px;
+  margin: 0 auto;
+  padding-top: calc(var(--section-gap) / 4);
+}
+.card {
+  margin: 0 auto;
+}
+</style>
