15b04d848be7cff016b2f553deea8d8174cef079
AndrewCrescencio <andrew_workstation@hotmail.com>
2024-10-18 15:44:41 -0300

    feat: icon components used in the project

---
+++ src/components/icons/IconDocumentsFileCertificate.vue
@@ -0,0 +1,54 @@
+<template>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M19.0029 9.99913V7.82623C19.0029 7.29601 18.7918 6.78679 18.4167 6.41164L15.5875 3.58246C15.2123 3.2073 14.7031 2.99622 14.1729 2.99622H6.99792C5.89245 2.99622 4.99708 3.89159 4.99708 4.99705V19.0029C4.99708 20.1083 5.89245 21.0037 6.99792 21.0037H13.0004"
+      stroke="currentColor"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M19.0029 7.9983H15.0012C14.449 7.9983 14.0008 7.55011 14.0008 6.99788V2.99622"
+      stroke="currentColor"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <circle
+      cx="18.0025"
+      cy="16.0016"
+      r="3.00125"
+      stroke="currentColor"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M19.7172 22.0041V18.4636"
+      stroke="currentColor"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M16.2878 18.4636V22.0041"
+      stroke="currentColor"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M16.2878 22.0041L18.0025 21.5749L19.7172 22.0041"
+      stroke="currentColor"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
