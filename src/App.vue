75bab8914dc1e6cbf28305fe2811eb7cd2c2ecc5
AndrewCrescencio <andrew_workstation@hotmail.com>
2024-10-19 12:28:51 -0300

    feat: TheHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,15 @@
+<script>
+import TheHeader from "@/components/TheHeader.vue";
+
+export default {
+  components: {
+    TheHeader,
+  },
+};
+</script>
 <template>
   <div id="app">
-    <header class="app-header">
-      <div class="app-header__wrapper">
-        <nav class="app-navbar">
-          <router-link to="/">Home</router-link>
-          <router-link to="/clients">Clientes</router-link>
-          <router-link to="/register">Cadastrar</router-link>
-        </nav>
-      </div>
-    </header>
+    <the-header />
     <router-view />
   </div>
 </template>
-
-<style lang="scss" scoped>
-.app-header {
-  background-color: var(--color-background-soft);
-  &__wrapper {
-    width: 100%;
-    max-width: 1440px;
-    margin: 0 auto;
-  }
-}
-
-.app-navbar {
-  padding: 0 16px;
-  width: 100%;
-  height: 64px;
-  display: flex;
-  align-items: center;
-  gap: 16px;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <header class="app-header">
+    <div class="app-header__wrapper">
+      <nav class="app-navbar">
+        <router-link to="/">Home</router-link>
+        <router-link to="/clients">Clientes</router-link>
+        <router-link to="/register">Cadastrar</router-link>
+      </nav>
+    </div>
+  </header>
+</template>
+<style lang="scss" scoped>
+.app-header {
+  background-color: var(--color-background-soft);
+  &__wrapper {
+    width: 100%;
+    max-width: 1440px;
+    margin: 0 auto;
+  }
+}
+
+.app-navbar {
+  padding: 0 16px;
+  width: 100%;
+  height: 64px;
+  display: flex;
+  align-items: center;
+  gap: 16px;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+
+  &.router-link-exact-active {
+    color: var(--text-brand-contrast);
+  }
+}
+</style>
