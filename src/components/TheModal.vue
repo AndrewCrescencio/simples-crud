1085877b7e6ca33fb68c63b8dff4a4126febffad
AndrewCrescencio <andrew_workstation@hotmail.com>
2024-10-19 23:15:32 -0300

    feat: AppModal TODO: ajustar css e "integração" com ClientsView

---
+++ src/components/TheModal.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+  methods: {
+    openModal() {
+      this.$refs.modal.showModal();
+    },
+    closeModal() {
+      this.$refs.modal.close();
+    },
+  },
+};
+</script>
+
+<template>
+  <transition name="modal">
+    <dialog ref="modal" class="modal">
+      <div class="modal-header">
+        <slot name="header"> default header </slot>
+        <button class="modal-close-button" @click="closeModal">X</button>
+      </div>
+
+      <div class="modal-body">
+        <slot name="body"> default body </slot>
+      </div>
+
+      <div class="modal-footer">
+        <slot name="footer">
+          <button class="modal-default-button" @click="closeModal">OK</button>
+        </slot>
+      </div>
+    </dialog>
+  </transition>
+</template>
+<style lang="scss" scoped>
+.modal {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  z-index: 1000;
+  min-width: 300px;
+  max-width: 600px;
+  width: 100%;
+}
+</style>
--- src/views/ClientsView.vue
+++ src/views/ClientsView.vue
@@ -1,18 +1,44 @@
 <script>
 import TableCustomers from "@/components/TableCustomers.vue";
+import TheModal from "@/components/TheModal.vue";
 
 export default {
-  components: { TableCustomers },
+  components: { TableCustomers, TheModal },
+  methods: {
+    openModal() {
+      console.log("openModal was called");
+      this.$refs.modal.openModal();
+    },
+    closeModal() {
+      console.log("closeModal was called");
+      this.$refs.modal.closeModal();
+    },
+  },
 };
 </script>
 <template>
   <main>
-    <h2 class="title">Clientes</h2>
+    <div class="header">
+      <h2 class="title">Clientes</h2>
+      <button @click="openModal">Adicionar cliente</button>
+    </div>
     <TableCustomers />
+    <TheModal ref="modal">
+      <template v-slot:header> Custom Header </template>
+      <template v-slot:body> This is a custom body content. </template>
+      <!-- <template v-slot:footer>
+        <button @click="closeModal">Close</button>
+      </template> -->
+    </TheModal>
   </main>
 </template>
 <style lang="scss" scoped>
 main {
+  .header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
   max-width: 1440px;
   margin: 0 auto;
   padding: 0 16px;
